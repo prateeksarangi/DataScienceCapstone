c4eb750cee5a1e976141e250c192861d6cefcd93
Prateek Sarangi <sarangiprateek80@gmail.com>
2020-04-14 22:14:40 +0530

    Update InitialTesting.R

--- InitialTesting.R
+++ InitialTesting.R
@@ -1,3 +1,37 @@
 con <- file("final/en_US/en_US.twitter.txt", "r")
-readLines(con, 5)
+readLines(con, -1)
 close(con)
+
+readLines("Textname.txt",encoding="UTF-8")
+scan("Textname.txt","character",sep="\n")
+
+strsplit(string,"")
+table(sentences)
+
+sentences<-scan("final/en_US/en_US.twitter.txt","character",sep="\n")
+sentences<-gsub("\\.","",sentences)
+sentences<-gsub("\\,","",sentences)
+words<-strsplit(sentences," ")
+words.freq<-table(unlist(words))
+
+wordFrq <- cbind.data.frame(names(words.freq),as.integer(words.freq))
+
+
+library(tau) 
+library(data.table)
+
+createNgram <-function(stringVector, ngramSize){
+  ngram <- data.table()
+  ng <- textcnt(stringVector, method = "string", n=ngramSize, tolower = FALSE)
+  
+  if(ngramSize==1){
+    ngram <- data.table(w1 = names(ng), freq = unclass(ng), length=nchar(names(ng)))  
+  }
+  else {
+    ngram <- data.table(w1w2 = names(ng), freq = unclass(ng), length=nchar(names(ng)))
+  }
+  
+  return(ngram)
+}
+
+res <- createNgram(sentences, 2)
