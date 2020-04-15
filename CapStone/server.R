4e1ba80153a28c42d519586b79baf4b5c3fa70b7
prateeksarangi <sarangiprateek80@gmail.com>
2020-04-16 05:18:24 +0530

    Server code added

--- CapStone/server.R
+++ CapStone/server.R
@@ -1,26 +1,11 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+
+source("./wordPredictor.R")
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
-    })
-
+    
+    prediction <- reactive({predict_next_word(input$InputSentence)})
+    output$predictedWord <- renderText(prediction())
+    
 })
